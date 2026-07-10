08d503219c513bd9b5a7d34ef62958f23e5d754e
Lothamikk <morozikmorozov33@gmail.com>
2026-07-11 02:14:18 +0300

    add docker and render

--- DarkPortal/obj/Debug/net10.0/DarkPortal.AssemblyInfo.cs
+++ DarkPortal/obj/Debug/net10.0/DarkPortal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DarkPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aee3f46c3a48cd197cf0d5c71bd48276fc6e799")]
 [assembly: System.Reflection.AssemblyProductAttribute("DarkPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DarkPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
